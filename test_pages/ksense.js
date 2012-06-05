3d55c3cb6caf8a64104cf4e38bebe0e11dee9d0d
Jack Feser <jack.feser@gmail.com>
2012-06-05 17:09:21 -0500

    Prepare gesture code for merging into master

--- test_pages/ksense.js
+++ test_pages/ksense.js
@@ -121,4 +121,86 @@ var ksensejs = {
     get_delta_time : function() {
         return ksensejs.plugin().getDeltaTime();
     }
+
+    gesture : {
+        skeleton_position_history : [],
+        skeleton_velocity_history : [],
+        timeframes : 30,
+
+        swipe : function(PlayerID, joint, gestureLength, gestureSpeed)
+        //checks to see if player is swiping (sustained velocity in x or y axis for 3 frames)
+        //two optional functions, length and speed (default length is 2, default speed is .075)
+        {
+	    if(!gestureLength)
+	        var gestureLength = 2
+	    if(!gestureSpeed)
+	        var gestureSpeed = .075;
+	    var countx=0;
+	    var county=0;
+
+	    var data = get_past_data(PlayerID, joint, gestureLength, ksensejs.gesture.skeleton_velocity_history);
+	    if(data.length !== gestureLength)
+	        return "none";
+
+	    for(i = 0; i < gestureLength; i++)
+	    {
+	        if(data[i].x > gestureSpeed)
+		    countx++;
+	        if(data[i].x < -gestureSpeed)
+		    countx--;
+	        if(data[i].y > gestureSpeed)
+		    county++;
+	        if(data[i].y < -gestureSpeed)
+		    county--;
+	    }
+
+	    if(countx === gestureLength && county === 0)
+	        return "right";
+	    if(countx === -gestureLength && county === 0)
+	        return "left";
+	    if(county === gestureLength && countx === 0)
+	        return "up";
+	    if(county === -gestureLength && countx === 0)
+	        return "down";
+	    return "none";
+        },
+
+        click : function(playerID, jointName, gestureLength, movementDistance, positionTolerance)
+        //checks for a click gesture from the player
+        //the gesture is pushing your hand forward, then back
+        {
+	    if(!gestureLength)
+	        var gestureLength = 2; //length will be multiplied by 2 to get full gesture, this would jsut be for the outer push
+	    if(!movementDistance)
+	        var movementDistance = .04;
+	    if(!positionTolerance)
+	        var positionTolerance = .1;
+	    var data = get_past_data(playerID, jointName, gestureLength*2, ksensejs.gesture.skeleton_position_history);
+	    if(data.length !== gestureLength*2)
+	        return false;
+	    var xdist = Math.abs(data[gestureLength*2-1].x-data[0].x);
+	    var ydist = Math.abs(data[gestureLength*2-1].y-data[0].y);
+	    var zdist = Math.abs(data[gestureLength*2-1].z-data[0].z);
+	    var check_click = data[gestureLength].z-data[2*gestureLength-1].z;
+	    if (check_click < -movementDistance && xdist < positionTolerance && ydist < positionTolerance && zdist < positionTolerance)
+	    {
+	        return true;
+	    }
+	    return false;
+        },
+
+        function get_past_data(id, joint, timeframes, array)
+        //returns an array of all joint data (either position or velocity) from an id
+        //returns a number of timeframes, starting at the present and going back
+        {
+            var frame_count = Math.min(timeframes,array.length);
+            var past_data = [];
+            for(i = 0; i < frame_count; i++) {
+	        if(typeof(array[i][id]) === "object") {
+	            past_data.push(array[i][id][joint]);
+                }
+            }
+            return past_data;
+        }
+    }
 };
