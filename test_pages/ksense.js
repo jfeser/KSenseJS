4e380ca70fb3e604d415327ddff1481ca05eb6f3
Jack Feser <jack.feser@gmail.com>
2012-07-18 15:36:43 -0500

    Add closure compiler annotations.

--- test_pages/ksense.js
+++ test_pages/ksense.js
@@ -1,8 +1,79 @@
-var ksensejs = {
+// ==ClosureCompiler==
+// @compilation_level SIMPLE_OPTIMIZATIONS
+// @output_file_name ksense_compressed.js
+// ==/ClosureCompiler==
+
+"use strict";
+
+var ksensejs = {};
+
+/** @typedef {{x: number, y: number, z: number}} */
+ksensejs.Vector;
+
+/** @typedef {{x: number, y: number, z: number, mag: number}} */
+ksensejs.MagVector;
+
+/** @typedef {{ankle_left: ksensejs.Vector,
+               hip_left: ksensejs.Vector,
+               foot_right: ksensejs.Vector,
+               shoulder_left: ksensejs.Vector,
+               ankle_right: ksensejs.Vector,
+               hip_right: ksensejs.Vector,
+               hand_left: ksensejs.Vector,
+               shoulder_right: ksensejs.Vector,
+               elbow_left: ksensejs.Vector,
+               knee_left: ksensejs.Vector,
+               hand_right: ksensejs.Vector,
+               spine: ksensejs.Vector,
+               elbow_right: ksensejs.Vector,
+               knee_right: ksensejs.Vector,
+               head: ksensejs.Vector,
+               wrist_left: ksensejs.Vector,
+               foot_left: ksensejs.Vector,
+               shoulder_center: ksensejs.Vector,
+               hip_center: ksensejs.Vector,
+               wrist_right: ksensejs.Vector}} */
+ksensejs.SkeletonData;
+
+/** @typedef {{ankle_left: ksensejs.MagVector,
+               hip_left: ksensejs.MagVector,
+               foot_right: ksensejs.MagVector,
+               shoulder_left: ksensejs.MagVector,
+               ankle_right: ksensejs.MagVector,
+               hip_right: ksensejs.MagVector,
+               hand_left: ksensejs.MagVector,
+               shoulder_right: ksensejs.MagVector,
+               elbow_left: ksensejs.MagVector,
+               knee_left: ksensejs.MagVector,
+               hand_right: ksensejs.MagVector,
+               spine: ksensejs.MagVector,
+               elbow_right: ksensejs.MagVector,
+               knee_right: ksensejs.MagVector,
+               head: ksensejs.MagVector,
+               wrist_left: ksensejs.MagVector,
+               foot_left: ksensejs.MagVector,
+               shoulder_center: ksensejs.MagVector,
+               hip_center: ksensejs.MagVector,
+               wrist_right: ksensejs.MagVector}} */
+ksensejs.VelocityData;
+
+ksensejs = {
+    /** @type{?Object.<number, ksensejs.SkeletonData>} 
+     * @private
+     */
     skeleton_data : null,
+
+    /** @type{?Object.<number, ksensejs.VelocityData>} 
+     * @private
+     */
     velocity_data : null,
+
+    /** @type{?function()} 
+     * @private
+     */
     callback : null,
 
+    /** Initialize the plugin object and start detection. */
     initialize : function() {
         // Create a plugin object and append to the <body> element
         var plugin_object = document.createElement('object');
@@ -14,10 +85,16 @@ var ksensejs = {
         ksensejs.initialized = true;
     },
 
+    /** Get a reference to the plugin object.
+     * @private
+     */
     plugin : function() {
         return document.getElementById('plugin0');
     },
 
+    /** Handle new data from the plugin.  Also run the user supplied callback.
+     * @private
+     */
     handle_new_data : function() {
         ksensejs.skeleton_data = ksensejs.plugin().getSkeletonData();
         ksensejs.velocity_data = ksensejs.plugin().getVelocityData();
@@ -27,13 +104,22 @@ var ksensejs = {
         }
     },
 
+    /** Check whether the plugin is tracking anything. 
+     * @return {boolean}
+     */
     is_tracking : function() {
+        if(ksensejs.skeleton_data == null) {
+            return false;
+        }
         if( Object.keys(ksensejs.skeleton_data).length > 0 ) {
             return true;
         }
         return false;
     },
 
+    /** Get a list of all valid tracking IDs.
+     * @return {Array.<number>}
+     */
     get_tracking_ids : function() {
         if( ksensejs.skeleton_data === null ) {
             return [];
@@ -42,6 +128,10 @@ var ksensejs = {
         }
     },
 
+    /** Get all the skeleton data for the given ID.
+     * @param {number} id
+     * @return {ksensejs.SkeletonData}
+     */
     get_skeleton_data : function(id) {
         if( typeof ksensejs.skeleton_data[id] === "undefined" ) {
             throw "Invalid tracking ID.";
@@ -50,6 +140,10 @@ var ksensejs = {
         }
     },
 
+    /** Get all the velocity data for the given ID.
+     * @param {number} id
+     * @return {ksensejs.VelocityData}
+     */
     get_velocity_data : function(id) {
         if( typeof ksensejs.velocity_data[id] === "undefined" ) {
             throw "Invalid tracking ID.";
@@ -58,6 +152,11 @@ var ksensejs = {
         }
     },
 
+    /** Get the location of the input joint for the input ID.
+     * @param {number} id
+     * @param {string} name
+     * @return {ksensejs.Vector}
+     */
     get_joint_location : function(id, name) {
         if( typeof ksensejs.skeleton_data[id] === "undefined" ) {
             throw "Invalid tracking ID.";
@@ -66,38 +165,55 @@ var ksensejs = {
         }
     },
 
+    /** Get the velocity magnitude of the input joint for the input ID.
+     * @param {number} id
+     * @param {string} name
+     * @return {number}
+     */
     get_joint_velocity : function(id, name) {
         if( typeof ksensejs.velocity_data[id] === "undefined" ) {
             throw "Invalid tracking ID.";
         } else {
-            return ksensejs.velocity_data[id][name]["mag"];
+            return ksensejs.velocity_data[id][name].mag;
         }
     },
 
+    /** Get the velocity vector of the input joint for the input ID.
+     * @param {number} id
+     * @param {string} name
+     * @return {ksensejs.Vector}
+     */
     get_joint_velocity_vector : function(id, name) {
         if( typeof ksensejs.velocity_data[id] === "undefined" ) {
             throw "Invalid tracking ID.";
         } else {
-            return {x:ksensejs.velocity_data[id][name]["x"],
-                    y:ksensejs.velocity_data[id][name]["y"],
-                    z:ksensejs.velocity_data[id][name]["z"]};
+            return {x:ksensejs.velocity_data[id][name].x,
+                    y:ksensejs.velocity_data[id][name].y,
+                    z:ksensejs.velocity_data[id][name].z};
         }
     },
 
+    /** Set the new data callback.
+     * @param {function()} callback
+     */
     set_new_data_callback : function(callback) {
         if( typeof callback === "function" ) {
             ksensejs.callback = callback;
         }
     },
 
+    /** Remove the new data callback. */
     unset_new_data_callback : function() {
         ksensejs.callback = null;
     },
 
+    /** Scale a vector from the Kinect to a scaled frame.
+     * @param {ksensejs.Vector} init_coord
+     * @param {ksensejs.Vector} scale_coord
+     * @return {ksensejs.Vector}
+     */
     scale_position : function(init_coord, scale_coord) {
-        var x = init_coord.x;
-        var y = init_coord.y;
-        var z = init_coord.z;
+        var x = init_coord.x, y = init_coord.y, z = init_coord.z;
 
         x += 2.2;
         if ( x < 0 ) {
@@ -118,89 +234,205 @@ var ksensejs = {
         return {x:x, y:y, z:z};
     },
 
+    /** Get the time between the last two frames of data.
+     * @return {number}
+     */
     get_delta_time : function() {
         return ksensejs.plugin().getDeltaTime();
-    }
+    },
 
     gesture : {
         skeleton_position_history : [],
         skeleton_velocity_history : [],
         timeframes : 30,
 
+        normalize_player: function (id, joint, dataSource, frame) {
+            if (!dataSource) {
+                dataSource = ksensejs.gesture.skeleton_position_history;
+            }
+
+            if (!frame) {
+                frame = 0;
+            }
+
+            var playerFrame = dataSource[frame][id];
+
+            if (joint === 'all') {
+                joint = [];
+                var i;
+                for (i in playerFrame) {
+                    if (playerFrame.hasOwnProperty(i)) {
+                        joint.push(i);
+                    }
+                }
+            }
+
+            if (typeof(joint) !== 'object') {
+                joint = [joint];
+            }
+
+            //shoulders can rotate around +-57deg, hips only around +-45deg.
+            var leftPt = ksensejs.get_joint_location(id, 'shoulder_left');
+            var rightPt = ksensejs.get_joint_location(id, 'shoulder_right');
+
+            if(dataSource === ksensejs.gesture.skeleton_velocity_history) {
+                var centerPt = {x:0, y:0, z:0};
+            } else {
+                var centerPt = playerFrame.hip_center;
+            }
+
+            //returns an angle with 0rad being facing the Kinect
+            var angle = Math.atan((rightPt.z - leftPt.z) / (rightPt.x - leftPt.x));
+            var sin = Math.sin(-angle), cos = Math.cos(-angle), retObj = {}, a = 0;
+
+            //if a velocity used, we must modify how we pull the data (individual axes are 1-4, not 0-3).
+            if (playerFrame[joint[0]].length === 4) {
+                a = 1;
+            }
+
+            var jointLoc, x, z;
+            for (var i = 0; i < joint.length; i++) {
+                jointLoc = playerFrame[joint[i]];
+                x = jointLoc[a] - centerPt.x;
+                z = jointLoc[a + 2] - centerPt.z;
+
+                if(a === 1) {
+                    retObj[joint[i]] = [jointLoc[0],
+                                        x * cos - z * sin + centerPt.x,
+                                        jointLoc[2],
+                                        x * sin + z * cos + centerPt.z];
+                } else {
+                    retObj[joint[i]] = [x * cos - z * sin + centerPt.x,
+                                        jointLoc[1],
+                                        x * sin + z * cos + centerPt.z];
+                }
+            }
+            return retObj;
+        },
+
         swipe : function(PlayerID, joint, gestureLength, gestureSpeed)
         //checks to see if player is swiping (sustained velocity in x or y axis for 3 frames)
         //two optional functions, length and speed (default length is 2, default speed is .075)
         {
-	    if(!gestureLength)
-	        var gestureLength = 2
-	    if(!gestureSpeed)
-	        var gestureSpeed = .075;
-	    var countx=0;
-	    var county=0;
+	    if(!gestureLength) {
+	        gestureLength = 2;
+            }
 
-	    var data = get_past_data(PlayerID, joint, gestureLength, ksensejs.gesture.skeleton_velocity_history);
-	    if(data.length !== gestureLength)
+	    if(!gestureSpeed) {
+	        gestureSpeed = .075;
+            }
+
+	    var countx = 0;
+	    var county = 0;
+	    var data = get_past_data(PlayerID,
+                                     joint,
+                                     gestureLength,
+                                     ksensejs.gesture.skeleton_velocity_history,
+                                     normalize);
+
+	    if(data.length !== gestureLength) {
 	        return "none";
+            }
 
-	    for(i = 0; i < gestureLength; i++)
-	    {
-	        if(data[i].x > gestureSpeed)
+	    for(var i = 0; i < gestureLength; i++) {
+	        if(data[i].x > gestureSpeed) {
 		    countx++;
-	        if(data[i].x < -gestureSpeed)
+                }
+                if(data[i].x < -gestureSpeed) {
 		    countx--;
-	        if(data[i].y > gestureSpeed)
+                }
+	        if(data[i].y > gestureSpeed) {
 		    county++;
-	        if(data[i].y < -gestureSpeed)
+                }
+	        if(data[i].y < -gestureSpeed) {
 		    county--;
+                }
 	    }
 
-	    if(countx === gestureLength && county === 0)
+	    if(countx === gestureLength && county === 0) {
 	        return "right";
-	    if(countx === -gestureLength && county === 0)
+            }
+	    if(countx === -gestureLength && county === 0) {
 	        return "left";
-	    if(county === gestureLength && countx === 0)
+            }
+	    if(county === gestureLength && countx === 0) {
 	        return "up";
-	    if(county === -gestureLength && countx === 0)
+            }
+	    if(county === -gestureLength && countx === 0) {
 	        return "down";
+            }
+
 	    return "none";
         },
 
-        click : function(playerID, jointName, gestureLength, movementDistance, positionTolerance)
+        click : function(playerID, jointName, normalize, gestureLength, movementDistance, positionTolerance)
         //checks for a click gesture from the player
         //the gesture is pushing your hand forward, then back
         {
 	    if(!gestureLength)
-	        var gestureLength = 2; //length will be multiplied by 2 to get full gesture, this would jsut be for the outer push
+                //length will be multiplied by 2 to get full gesture,
+                //this would jsut be for the outer push
+	        gestureLength = 2;
+
 	    if(!movementDistance)
-	        var movementDistance = .04;
+	        movementDistance = .04;
+
 	    if(!positionTolerance)
-	        var positionTolerance = .1;
-	    var data = get_past_data(playerID, jointName, gestureLength*2, ksensejs.gesture.skeleton_position_history);
+	        positionTolerance = .1;
+
+	    var data = get_past_data(playerID,
+                                     jointName,
+                                     gestureLength*2,
+                                     ksensejs.gesture.skeleton_position_history,
+                                     normalize);
+
 	    if(data.length !== gestureLength*2)
 	        return false;
-	    var xdist = Math.abs(data[gestureLength*2-1].x-data[0].x);
-	    var ydist = Math.abs(data[gestureLength*2-1].y-data[0].y);
-	    var zdist = Math.abs(data[gestureLength*2-1].z-data[0].z);
-	    var check_click = data[gestureLength].z-data[2*gestureLength-1].z;
-	    if (check_click < -movementDistance && xdist < positionTolerance && ydist < positionTolerance && zdist < positionTolerance)
-	    {
+
+	    var xdist = Math.abs(data[gestureLength * 2 - 1].x - data[0].x);
+	    var ydist = Math.abs(data[gestureLength * 2 - 1].y - data[0].y);
+	    var zdist = Math.abs(data[gestureLength * 2 - 1].z - data[0].z);
+	    var check_click = data[gestureLength].z - data[2 * gestureLength - 1].z;
+
+	    if (check_click < -movementDistance &&
+                xdist < positionTolerance &&
+                ydist < positionTolerance &&
+                zdist < positionTolerance)
 	        return true;
-	    }
+
 	    return false;
         },
 
-        function get_past_data(id, joint, timeframes, array)
+        get_past_data : function(id, joint, timeframes, array, normalize)
         //returns an array of all joint data (either position or velocity) from an id
         //returns a number of timeframes, starting at the present and going back
         {
-            var frame_count = Math.min(timeframes,array.length);
-            var past_data = [];
-            for(i = 0; i < frame_count; i++) {
-	        if(typeof(array[i][id]) === "object") {
-	            past_data.push(array[i][id][joint]);
+            if( typeof(normalize) === 'undefined' )
+                normalize = true;
+
+            var repeat = Math.min(timeframes, array.length);
+            var ret_arr = [];
+
+            for (var i = 0; i < repeat; i++) {
+                if (typeof(array[i][id]) === "object") {
+                    if(normalize) {
+                        ret_arr.push(ksensejs.gesture.normalize_player(id, joint, array, i)[joint]);
+                    } else {
+                        ret_arr.push(array[i][id][joint]);
+                    }
                 }
             }
-            return past_data;
+
+            return ret_arr;
         }
     }
 };
+
+ksensejs.set_new_data_callback(function () {
+                                   ksensejs.gesture.skeleton_position_history.unshift(ksensejs.skeleton_data);
+                                   ksensejs.gesture.skeleton_velocity_history.unshift(ksensejs.velocity_data);
+                                   if(ksensejs.gesture.skeleton_position_history.length > ksensejs.gesture.timeframes) {
+                                       ksensejs.gesture.skeleton_position_history.pop();
+                                       ksensejs.gesture.skeleton_velocity_history.pop();
+                                   }
+                               });
