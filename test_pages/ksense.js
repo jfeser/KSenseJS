99bc5b44b761d67571e8904b0d2a6a543f52d222
Jack Feser <jack.feser@gmail.com>
2012-07-30 11:26:17 -0500

    Remove closure compiler header.

--- test_pages/ksense.js
+++ test_pages/ksense.js
@@ -23,10 +23,6 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-// ==ClosureCompiler==
-// @compilation_level SIMPLE_OPTIMIZATIONS
-// @output_file_name ksense_compressed.js
-// ==/ClosureCompiler==
 
 "use strict";
 
